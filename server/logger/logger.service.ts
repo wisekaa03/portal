c15c812d62ae0e18bed9d29b68173719d9dad338
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-09-25 18:19:45 +0300

    helm chart service.yaml

--- server/logger/logger.service.ts
+++ server/logger/logger.service.ts
@@ -4,24 +4,46 @@
 import { Logger } from '@nestjs/common';
 // #endregion
 
+const dev = process.env.NODE_ENV !== 'production';
+
 export class AppLogger extends Logger {
   log(message: any, context?: string): void {
-    super.log(message, context);
+    if (dev) {
+      super.log(message, context);
+    } else {
+      console.log(context, message);
+    }
   }
 
   error(message: any, trace?: string, context?: string): void {
-    super.error(message, trace, context);
+    if (dev) {
+      super.error(message, trace, context);
+    } else {
+      console.error(context, message, trace);
+    }
   }
 
   warn(message: any, context?: string): void {
-    super.warn(message, context);
+    if (dev) {
+      super.warn(message, context);
+    } else {
+      console.warn(context, message);
+    }
   }
 
   debug(message: any, context?: string): void {
-    super.debug(message, context);
+    if (dev) {
+      super.debug(message, context);
+    } else {
+      console.debug(context, message);
+    }
   }
 
   verbose(message: any, context?: string): void {
-    super.verbose(message, context);
+    if (dev) {
+      super.verbose(message, context);
+    } else {
+      console.info(context, message);
+    }
   }
 }
