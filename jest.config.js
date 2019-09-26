05bf83aaee4b6f84e9d1686b24236dfd9108473c
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-09-26 13:16:34 +0300

    jest config testMatch

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.svg$': 'jest-svg-transformer',
   },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/!(target)/?(*.)+(spec|test).[jt]s?(x)'],
   setupFiles: ['./jest.setup.ts'],
   testPathIgnorePatterns: ['/.next/', '/.nest/', '/node_modules/'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
