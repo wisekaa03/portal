4e19fb9c2a0fa763dd9daa23cb126db7a9d2ff9e
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-09-26 11:05:21 +0300

    gitlab-ci, jest testing config, helm nodePort

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.svg$': 'jest-svg-transformer',
   },
-  testMatch: ['**/__tests__/*.(ts|tsx)'],
+  testMatch: ['**/__tests__/*.(ts|tsx)', '**/*.spec.ts'],
   setupFiles: ['./jest.setup.ts'],
   testPathIgnorePatterns: ['./.next/', './node_modules/'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
