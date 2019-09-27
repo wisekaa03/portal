cbcc6875146bbfd99c81aa7f0a7c70ec1b8167d7
Stanislav V Vyaliy (WiseKaa) <wisekaa03@gmail.com>
2019-09-27 18:43:25 +0300

    jest timeout

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @format */
 
 module.exports = {
-  testTimeout: 10000,
+  testTimeout: 100000,
   preset: 'ts-jest', // 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
