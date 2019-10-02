ec5e6a4110035269eff5083adbd692bd58ab6350
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-02 15:09:19 +0300

    node 10 -> 12

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
   apps: [
     {
       name: 'Portal',
-      script: '.next/production-server/main.js',
+      script: '.nest/main.js',
       instances: 1,
       autorestart: true,
       watch: false,
