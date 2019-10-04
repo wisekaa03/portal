57cccdb3f0373a59dcf468ea4ef26f79cbba488a
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-04 11:25:04 +0300

    next-images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const { join, resolve } = require('path');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 // const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const optimizedImages = require('next-optimized-images');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withFonts = require('next-fonts');
@@ -141,6 +142,37 @@ function HACKremoveMinimizeOptionFromCssLoaders(config) {
 }
 
 const plugins = [
+  [
+    optimizedImages,
+    {
+      // these are the default values so you don't have to provide them if they are good enough for your use-case.
+      // but you can overwrite them here with any valid value you want.
+      inlineImageLimit: 8192,
+      imagesFolder: 'images',
+      imagesName: '[name]-[hash].[ext]',
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+      optimizeImages: true,
+      optimizeImagesInDev: false,
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+      svgo: {
+        // enable/disable svgo plugins here
+      },
+      webp: {
+        preset: 'default',
+        quality: 75,
+      },
+    },
+  ],
   [
     withCSS,
     {
