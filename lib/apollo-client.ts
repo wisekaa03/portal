b27f82fd1233477f69019a4bbbf866edfc5b22ef
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-09 09:35:31 +0300

    apollo-client

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -4,7 +4,7 @@
 import fetch from 'isomorphic-fetch';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { ApolloLink, Observable, FetchResult /* , split */ } from 'apollo-link';
+import { concat, ApolloLink } from 'apollo-link';
 import { onError } from 'apollo-link-error';
 import { setContext } from 'apollo-link-context';
 import { HttpLink, createHttpLink } from 'apollo-link-http';
@@ -29,24 +29,49 @@ export const apolloClient = (
   const cache = new InMemoryCache({
     dataIdFromObject: (object: NodeIdGetterObj) => object.nodeId || null,
   }).restore(initialState);
-  let link: ApolloLink;
+
+  // Create an http link:
+  let httpLink: ApolloLink;
+
+  const authLink = setContext((_, { headers }) => {
+    const token = getStorage('token');
+
+    // eslint-disable-next-line no-debugger
+    // debugger;
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const errorLink = onError(({ graphQLErrors, networkError }): any => {
+    if (graphQLErrors) {
+      // TODO: реализовать https://github.com/apollographql/apollo-link/tree/master/packages/apollo-link-error
+      graphQLErrors.map(({ message, locations, path }): any =>
+        console.error('[GraphQL error]: Path:', path, 'Message:', message, 'Location:', locations),
+      );
+    }
+    if (networkError) {
+      console.error('[Network error]:', networkError);
+    }
+  });
 
   if (!__SERVER__) {
     // const subscriptionsUri = `${window.location.origin.replace(
     //   'http',
     //   'ws',
     // )}/graphql`; // __WEBSOCKET_URI__
-
     // Create a WebSocket link:
     // const wsLink = new WebSocketLink({
     //   uri: subscriptionsUri,
     //   options: {
     //     reconnect: true,
-
     //     // connectionParams: async () => {
     //     //   return { token: localStorage.getItem('token') };
     //     // },
-
     //     connectionCallback: (errors: Error[], _result: any): any => {
     //       if (errors) {
     //         console.error('[Error in webSocket]:', errors);
@@ -55,62 +80,14 @@ export const apolloClient = (
     //   },
     // });
 
-    // Create an http link:
-    const httpLink = createHttpLink({
+    httpLink = createHttpLink({
       uri: `/graphql`,
     });
-
-    const authLink = setContext((_, { headers }) => {
-      const token = getStorage('token');
-
-      // eslint-disable-next-line no-debugger
-      // debugger;
-
-      return {
-        headers: {
-          ...headers,
-          authorization: token ? `Bearer ${token}` : '',
-        },
-      };
-    });
-
-    // link = ApolloLink.from([
-    //   onError(({ graphQLErrors, networkError }): any => {
-    //     if (graphQLErrors) {
-    //       graphQLErrors.map(({ message, locations, path }): any =>
-    //         console.error('[GraphQL error]: Path:', path, 'Message:', message, 'Location:', locations),
-    //       );
-    //     }
-    //     if (networkError) {
-    //       console.error('[Network error]:', networkError);
-    //     }
-    //   }),
-
-    //   // split(
-    //   //   ({ query }) => {
-    //   //     const definition = getMainDefinition(query);
-    //   //     return (
-    //   //       definition.kind === 'OperationDefinition' &&
-    //   //       definition.operation === 'subscription'
-    //   //     );
-    //   //   },
-    //   //   wsLink,
-    //   httpLink,
-    //   // ),
-    // ]);
-    link = authLink.concat(httpLink);
   } else {
     global.fetch = fetch;
 
-    link = onError(({ graphQLErrors, networkError /* , operation, forward */ }): Observable<FetchResult> | void => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }): any =>
-          console.error(`[GraphQL error]: Path: ${path}`, `Message:`, message, `Location:`, locations),
-        );
-      }
-      if (networkError) {
-        console.error(`[Network error]:`, networkError);
-      }
+    httpLink = createHttpLink({
+      uri: `http://${process.env.HOST}:${process.env.PORT}/graphql`,
     });
   }
 
@@ -118,7 +95,7 @@ export const apolloClient = (
     apollo = new ApolloClient({
       connectToDevTools: !__SERVER__,
       ssrMode: __SERVER__,
-      link,
+      link: concat(authLink.concat(errorLink), httpLink),
       cache,
       resolvers: stateLinkResolvers,
     });
