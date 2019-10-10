9c13547f474acae10bf136dcce36ab999b17b0da
Roman Tikhiy <r.tikhiy@i-npz.ru>
2019-10-10 15:59:22 +0300

    Profile Popover alpha

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -1,19 +1,36 @@
 /** @format */
 
 // #region Imports NPM
-import React from 'react';
+import React, { useState } from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Hidden, /* Button, */ IconButton, Avatar } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Hidden,
+  Popover,
+  Paper,
+  Box,
+  /* Button, */ IconButton,
+  Avatar,
+  Typography,
+} from '@material-ui/core';
+import clsx from 'clsx';
 import MenuIcon from '@material-ui/icons/Menu';
+import PhoneIcon from '@material-ui/icons/Phone';
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import { blue } from '@material-ui/core/colors';
 // import Link from 'next/link';
 // #endregion
 // #region Imports Local
 // import { UserContext } from '../lib/types';
 import HeaderBg from '../public/images/jpeg/header_bg.jpg';
+import PopoverBg from '../public/images/png/profile_popover_bg.png';
 import LogoMin from '../public/images/png/logo-min.png';
 // #endregion
 
 export const appBarHeight = 64;
+const avatarHeight = 48;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,6 +52,37 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    avatar: {
+      background: blue[400],
+      height: avatarHeight,
+      width: avatarHeight,
+    },
+    profile: {
+      padding: theme.spacing() / 2,
+      background: `url(${PopoverBg})`,
+      minWidth: '200px',
+      minHeight: '150px',
+      display: 'grid',
+      gridTemplateColumns: `200px ${avatarHeight}px`,
+      borderRadius: theme.spacing() / 2,
+    },
+    pointer: {
+      cursor: 'pointer',
+    },
+    avatarWrap: {
+      padding: theme.spacing() / 2,
+    },
+    profileName: {
+      margin: theme.spacing(),
+      fontSize: '16px',
+    },
+    phoneBlock: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 5fr',
+      gridGap: theme.spacing(),
+      padding: theme.spacing(),
+      alignItems: 'center',
+    },
   }),
 );
 
@@ -44,8 +92,19 @@ interface AppBarProps {
 
 export default (props: AppBarProps): React.ReactElement => {
   const classes = useStyles({});
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   // const user = useContext(UserContext);
   const { handleDrawerOpen } = props;
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = (): void => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   return (
     <AppBar id="header" position="sticky" className={classes.root}>
@@ -64,7 +123,36 @@ export default (props: AppBarProps): React.ReactElement => {
         <div className={classes.logo}>
           <img src={LogoMin} alt="logo" />
         </div>
-        <Avatar />
+        <Box className={classes.avatarWrap} onClick={handlePopoverOpen}>
+          <Avatar className={clsx(classes.avatar, classes.pointer)}>И</Avatar>
+        </Box>
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handlePopoverClose}
+          classes={{ paper: classes.profile }}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          marginThreshold={0}
+          disableRestoreFocus
+        >
+          <Typography className={classes.profileName}>Иванов Иван Иванович</Typography>
+          <Avatar className={classes.avatar}>И</Avatar>
+          <Box className={classes.phoneBlock}>
+            <PhoneIcon />
+            <Typography>+ 7 (999) 1234567</Typography>
+            <PhoneIphoneIcon />
+            <Typography>+ 7 (999) 7654321</Typography>
+            <PhoneInTalkIcon />
+            <Typography>1234</Typography>
+          </Box>
+        </Popover>
       </Toolbar>
     </AppBar>
   );
