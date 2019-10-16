8ecb483cb28a016e5af7c174e5f6b463f84dae54
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-16 14:37:17 +0300

    user test

--- server/user/user.resolver.spec.ts
+++ server/user/user.resolver.spec.ts
@@ -1,41 +1,38 @@
 /** @format */
 
 // #region Imports NPM
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
-import { I18nModule, QueryResolver, HeaderResolver } from 'nestjs-i18n';
 import { JwtService, JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // #endregion
 // #region Imports Local
-import { UserService } from './user.service';
-import { ConfigModule } from '../config/config.module';
-import { UserEntity } from './user.entity';
-import { LoggerModule } from '../logger/logger.module';
-import { LdapModule } from '../ldap/ldap.module';
-import { Scope } from '../ldap/interfaces/ldap.interface';
-import { ConfigService } from '../config/config.service';
+import { I18nModule } from 'nestjs-i18n';
+import { UserResolver } from './user.resolver';
+import { UserModule } from './user.module';
 import { LogService } from '../logger/logger.service';
 import { LogServiceMock } from '../../__mocks__/logger.service.mock';
-import { JwtServiceMock } from '../../__mocks__/jwt.service.mock';
-import { ProfileEntity } from '../profile/profile.entity';
-import { ProfileModule } from '../profile/profile.module';
-import { AuthModule } from '../auth/auth.module';
 import { AuthService } from '../auth/auth.service';
 import { AuthServiceMock } from '../../__mocks__/auth.service.mock';
+import { JwtServiceMock } from '../../__mocks__/jwt.service.mock';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 import { CookieSerializer } from '../auth/cookie.serializer';
 import { CookieSerializerMock } from '../../__mocks__/cookie.serializer.mock';
+import { UserService } from './user.service';
+import { UserServiceMock } from '../../__mocks__/user.service.mock';
 import { GqlAuthGuard } from '../guards/gqlauth.guard';
 import { GqlAuthGuardMock } from '../../__mocks__/gqlauth.guard.mock';
+import { JwtStrategy } from '../auth/strategies/jwt.strategy';
+import { JwtStrategyMock } from '../../__mocks__/jwt.strategy.mock';
 // #endregion
 
-describe('UserService', () => {
-  let service: UserService;
+describe('UserResolver', () => {
+  let resolver: UserResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        ConfigModule,
-        LoggerModule,
+        UserModule,
 
         I18nModule.forRootAsync({
           imports: [ConfigModule],
@@ -44,13 +41,10 @@ describe('UserService', () => {
             path: configService.i18nPath,
             filePattern: configService.i18nFilePattern,
             fallbackLanguage: configService.fallbackLanguage,
-            resolvers: [new QueryResolver(['lang', 'locale', 'l']), new HeaderResolver()],
           }),
         }),
 
         TypeOrmModule.forRoot({}),
-        TypeOrmModule.forFeature([UserEntity]),
-        TypeOrmModule.forFeature([ProfileEntity]),
 
         JwtModule.registerAsync({
           imports: [ConfigModule],
@@ -61,40 +55,28 @@ describe('UserService', () => {
             } as JwtModuleOptions;
           },
         }),
-
-        LdapModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: async (configService: ConfigService) => {
-            return {
-              url: configService.get('LDAP_URL'),
-              bindDN: configService.get('LDAP_BIND_DN'),
-              bindCredentials: configService.get('LDAP_BIND_PW'),
-              searchBase: configService.get('LDAP_SEARCH_BASE'),
-              searchFilter: configService.get('LDAP_SEARCH_FILTER'),
-              searchScope: 'sub' as Scope,
-              searchAttributes: ['*'],
-              reconnect: true,
-            };
-          },
-        }),
-
-        ProfileModule,
       ],
       providers: [
-        UserService,
+        UserResolver,
+        { provide: UserService, useValue: UserServiceMock },
         { provide: AuthService, useValue: AuthServiceMock },
         { provide: JwtService, useValue: JwtServiceMock },
       ],
     })
       .overrideProvider(LogService)
       .useValue(LogServiceMock)
+      .overrideProvider(JwtStrategy)
+      .useValue(JwtStrategyMock)
+      .overrideProvider(CookieSerializer)
+      .useValue(CookieSerializerMock)
+      .overrideGuard(GqlAuthGuard)
+      .useValue(GqlAuthGuardMock)
       .compile();
 
-    service = module.get<UserService>(UserService);
+    resolver = module.get<UserResolver>(UserResolver);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(resolver).toBeDefined();
   });
 });
