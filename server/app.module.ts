a2ae250981299c9fc34d9b514407d2d72778d51d
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-17 19:56:24 +0300

    Redis cache

--- server/app.module.ts
+++ server/app.module.ts
@@ -53,7 +53,7 @@ import { ProfileModule } from './profile/profile.module';
         host: configService.get('HTTP_REDIS_HOST'),
         port: parseInt(configService.get('HTTP_REDIS_PORT'), 10),
         db: configService.get('HTTP_REDIS_DB') ? parseInt(configService.get('HTTP_REDIS_DB'), 10) : undefined,
-        password: configService.get('REDIS_PASSWORD') ? configService.get('HTTP_REDIS_PASSWORD') : undefined,
+        password: configService.get('HTTP_REDIS_PASSWORD') ? configService.get('HTTP_REDIS_PASSWORD') : undefined,
         keyPrefix: configService.get('HTTP_REDIS_PREFIX') ? configService.get('HTTP_REDIS_PREFIX') : undefined,
         // retry_strategy: (options) => {}
       }),
