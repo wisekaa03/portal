6acc812d7026992e95f4e48d4ae31b4a2bc0bb3f
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-24 19:39:13 +0300

    controllers

--- server/controllers/controllers.module.ts
+++ server/controllers/controllers.module.ts
@@ -4,8 +4,6 @@
 import { Module } from '@nestjs/common';
 // #endregion
 // #region Imports Local
-import { NextModule } from '../next/next.module';
-import { LoggerModule } from '../logger/logger.module';
 import { AuthController } from './auth/auth.controller';
 import { HomeController } from './home/home.controller';
 import { PhonebookController } from './phonebook/phonebook.controller';
@@ -14,7 +12,7 @@ import { NewsController } from './news/news.controller';
 // #endregion
 
 @Module({
-  imports: [NextModule, LoggerModule],
+  imports: [],
   controllers: [HomeController, AuthController, PhonebookController, MailController, NewsController],
 })
 export class HomeModule {}
