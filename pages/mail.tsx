e07dcf9dad3c65908e3ec1b5032da55e114e6a8e
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-25 15:31:05 +0300

    pages -> mail

--- pages/mail.tsx
+++ pages/mail.tsx
@@ -3,11 +3,12 @@
 // #region Imports NPM
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Iframe from 'react-iframe';
+import Iframe from '../components/iframe';
 // #endregion
 // #region Imports Local
 import Page from '../layouts/main';
 import { includeDefaultNamespaces, nextI18next, I18nPage } from '../lib/i18n-client';
+
 // #endregion
 
 const useStyles = makeStyles(() =>
@@ -27,7 +28,7 @@ const Mail: I18nPage = (props): React.ReactElement => {
 
   return (
     <Page {...props}>
-      <Iframe className={classes.root} url={url} sandbox="allow-scripts" />
+      <Iframe className={classes.root} url={url} sandbox="allow-scripts allow-same-origin" />
     </Page>
   );
 };
