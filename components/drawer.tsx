8890561b45826d8ccdad4bf8f916663ababfabea
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-25 09:19:47 +0300

    drawer translation

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -81,18 +81,18 @@ const BaseDrawer: I18nPage<DrawerProps> = (props): React.ReactElement => {
 
   const urls = [
     { text: t('common:mail'), link: 'https://mail.kngk-group.ru', icon: <MailIcon /> },
-    { text: 'Адресная книга', link: '/phonebook', icon: <ImportContactsIcon /> },
-    { text: 'Личный кабинет', link: '/', icon: <AssignmentIndIcon /> },
+    { text: t('common:phonebook'), link: '/phonebook', icon: <ImportContactsIcon /> },
+    { text: t('common:profile'), link: '/', icon: <AssignmentIndIcon /> },
     {
-      text: 'Заявка в ИТ',
+      text: t('common:applicationit'),
       link: '/',
       icon: <Icon src={ItIcon} />,
     },
-    { text: 'Календарь компании', link: '/', icon: <Icon src={CalendarIcon} /> },
-    { text: 'База знаний', link: '/', icon: <LiveHelpIcon /> },
-    { text: 'Переговорные', link: '/', icon: <QuestionAnswerIcon /> },
-    { text: 'Лента новостей', link: '/news', icon: <DvrIcon /> },
-    { text: 'Настройки', link: '/', icon: <SettingsIcon /> },
+    { text: t('common:calendar'), link: '/', icon: <Icon src={CalendarIcon} /> },
+    { text: t('common:faq'), link: '/', icon: <LiveHelpIcon /> },
+    { text: t('common:meeting'), link: '/', icon: <QuestionAnswerIcon /> },
+    { text: t('common:timeline'), link: '/news', icon: <DvrIcon /> },
+    { text: t('common:settings'), link: '/', icon: <SettingsIcon /> },
   ];
 
   // TODO: подумать как правильнее разделять сервер и клиент при определении маршрута
