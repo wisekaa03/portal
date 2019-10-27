34f0de18ae4e356572d3b1c55cf5cfb3569dad7e
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-10-27 17:59:40 +0300

    pages - auth - login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,34 +3,32 @@
 // #region Imports NPM
 import React from 'react';
 import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { FetchResult } from 'apollo-link';
 import queryString from 'query-string';
 import Router from 'next/router';
 // #endregion
 // #region Imports Local
-import { LOGIN } from '../../lib/queries';
+import { LOGIN /* , CURRENT_USER */ } from '../../lib/queries';
 import LoginComponent from '../../components/login';
 import { includeDefaultNamespaces, I18nPage } from '../../lib/i18n-client';
-import { FIRST_PAGE } from '../../lib/constants';
 import { setStorage } from '../../lib/session-storage';
+import { Data } from '../../lib/types';
+import { FIRST_PAGE } from '../../lib/constants';
+import { UserResponse } from '../../server/user/models/user.dto';
 // #endregion
 
 const Login: I18nPage = (props): React.ReactElement => {
   const client = useApolloClient();
 
   const [login, { loading, error }] = useMutation(LOGIN, {
-    onCompleted(data) {
-      setStorage('token', data.login.token);
-      const { redirect = FIRST_PAGE } = queryString.parse(window.location.search);
+    update(_cache, { data }: FetchResult<Data<'data', UserResponse>>) {
+      setStorage('token', data ? data.login.token : '');
       client.resetStore();
-      // client.writeData({ data: { isLogin: true } });
 
-      Router.push({ pathname: redirect as string });
+      const { redirect = FIRST_PAGE } = queryString.parse(window.location.search);
+      Router.replace(redirect as string);
     },
   });
-
-  // if (loading) {
-  //   return <Loading type="linear" variant="indeterminate" />;
-  // }
 
   return <LoginComponent error={error} loading={loading} login={login} {...props} />;
 };
