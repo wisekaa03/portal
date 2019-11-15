3b847ef1f587918e6a8f26af52e2294996f60927
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-11-15 17:11:27 +0300

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -10,34 +10,6 @@ const entities = ['./.next/nest/**/*.entity.js'];
 // #endregion
 
 const configService = new Config.ConfigService(`${__dirname}/.env`);
-let logging = configService.get('DATABASE_LOGGING');
-
-if (logging === 'false') {
-  logging = false;
-} else if (logging === 'true') {
-  logging = true;
-} else {
-  logging = JSON.parse(logging);
-}
-
-const isCache = Boolean(configService.get('DATABASE_CACHE'));
-let cache;
-if (isCache) {
-  cache = {
-    type: 'redis',
-    options: {
-      host: configService.get('DATABASE_REDIS_HOST'),
-      port: configService.get('DATABASE_REDIS_PORT'),
-      // eslint-disable-next-line max-len
-      db: configService.get('DATABASE_REDIS_CACHE_DB') ? parseInt(configService.get('DATABASE_REDIS_CACHE_DB'), 10) : 0,
-      password: configService.get('DATABASE_REDIS_PASSWORD') ? configService.get('DATABASE_REDIS_PASSWORD') : undefined,
-      prefix: configService.get('DATABASE_REDIS_PREFIX') ? configService.get('DATABASE_REDIS_PREFIX') : undefined,
-    },
-    duration: configService.get('DATABASE_REDIS_TTL'),
-  };
-} else {
-  cache = false;
-}
 
 console.log('Database: Using ORMconfig.js...');
 
@@ -53,11 +25,11 @@ module.exports = {
   uuidExtension: 'pgcrypto',
   synchronize: configService.get('DATABASE_SYNCHRONIZE'),
   dropSchema: configService.get('DATABASE_DROP_SCHEMA'),
-  logging,
+  logging: 'all',
   logger: 'simple-console',
   entities,
   migrationsRun: configService.get('DATABASE_MIGRATIONS_RUN'),
-  cache,
+  cache: false,
   // migrations,
   // cli: {
   //   migrationsDir: 'migration',
