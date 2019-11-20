fcb3fb70fd3e007355975e2a29f2dcdb8717754a
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-11-20 08:09:21 +0300

    jest update

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,6 @@
 /** @format */
 /* eslint spaced-comment:0 */
-/// <reference types="./apps/portal/typings/global" />
+/// <reference types="./typings/global" />
 
 import * as enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
