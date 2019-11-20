a65387fc5a57b56a1b09fbcf206509d84b95ce37
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-11-20 12:53:15 +0300

    server - tests ldap

--- apps/portal/src/ldap/ldap.service.spec.ts
+++ apps/portal/src/ldap/ldap.service.spec.ts
@@ -7,8 +7,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 // #region Imports Local
 import { LdapService } from './ldap.service';
 import { LdapModule } from './ldap.module';
-import { LdapModuleOptions } from './ldap.interface';
+import { LdapModuleOptions, Scope, ldapADattributes } from './ldap.interface';
 import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 // #endregion
 
 describe('LdapService', () => {
@@ -17,10 +18,29 @@ describe('LdapService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        ConfigModule.register(resolve(__dirname, '../../..', '.env')),
+        ConfigModule.register(resolve(__dirname, '../../../..', '.env')),
 
         LdapModule.registerAsync({
-          useFactory: () => ({} as LdapModuleOptions),
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => {
+            return {
+              url: configService.get<string>('LDAP_URL'),
+              bindDN: configService.get<string>('LDAP_BIND_DN'),
+              bindCredentials: configService.get<string>('LDAP_BIND_PW'),
+              searchBase: configService.get<string>('LDAP_SEARCH_BASE'),
+              searchFilter: configService.get<string>('LDAP_SEARCH_FILTER'),
+              searchScope: 'sub' as Scope,
+              searchAttributes: ldapADattributes,
+              searchBaseAllUsers: configService.get<string>('LDAP_SEARCH_BASE_ALL_USERS'),
+              searchFilterAllUsers: configService.get<string>('LDAP_SEARCH_FILTER_ALL_USERS'),
+              searchScopeAllUsers: 'sub' as Scope,
+              searchAttributesAllUsers: ldapADattributes,
+              queueSize: 100,
+              reconnect: true,
+              cache: true,
+            } as LdapModuleOptions;
+          },
         }),
       ],
       providers: [],
