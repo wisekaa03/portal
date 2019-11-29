4a83d024106226598ada1be890126979e81462cb
Roman Tikhiy <r.tikhiy@i-npz.ru>
2019-11-29 11:47:54 +0300

    app: isAdmin fix type

--- apps/portal/components/isAdmin.tsx
+++ apps/portal/components/isAdmin.tsx
@@ -3,7 +3,7 @@
 import React, { useContext } from 'react';
 import { ProfileContext } from '../lib/context';
 
-const IsAdmin = ({ children }: any) => {
+const IsAdmin = ({ children }: any): undefined | React.ReactElement => {
   const profile = useContext(ProfileContext);
 
   return profile && profile.user && profile.user.isAdmin && <>{children}</>;
