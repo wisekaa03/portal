5afd3cd16b3277f6367870015cf12d93b754e577
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-12-04 18:12:44 +0300

    jest tests -> user resolver

--- apps/portal/src/user/user.resolver.spec.ts
+++ apps/portal/src/user/user.resolver.spec.ts
@@ -1,28 +1,25 @@
 /** @format */
 
 // #region Imports NPM
-import { resolve } from 'path';
+// import { resolve } from 'path';
 import { Test, TestingModule } from '@nestjs/testing';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 // #endregion
 // #region Imports Local
-import { ConfigModule } from '@app/config';
-import { LdapModule, LdapService, LdapModuleOptions } from '@app/ldap';
-import { ProfileModule } from '../profile/profile.module';
+// import { ConfigModule } from '@app/config';
+// import { LdapModule, LdapService, LdapModuleOptions } from '@app/ldap';
+// import { ProfileModule } from '../profile/profile.module';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 // #endregion
 
-jest.mock('@nestjs/typeorm/dist/typeorm.module');
-jest.mock('@app/ldap/ldap.service');
-jest.mock('./user.entity');
-jest.mock('../profile/profile.module');
-jest.mock('../profile/profile.service');
+// jest.mock('@nestjs/typeorm/dist/typeorm.module');
+// jest.mock('@app/ldap/ldap.service');
+// jest.mock('@app/config/config.service');
+// jest.mock('./user.entity');
+// jest.mock('../profile/profile.module');
+// jest.mock('../profile/profile.service');
 jest.mock('./user.service');
-
-const dev = process.env.NODE_ENV !== 'production';
-const test = process.env.NODE_ENV === 'test';
-const env = resolve(__dirname, dev ? (test ? '../../../..' : '../../..') : '../../..', '.env');
 
 describe('UsersResolver', () => {
   let resolver: UserResolver;
@@ -30,21 +27,18 @@ describe('UsersResolver', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        ConfigModule.register(env),
-
-        LdapModule.registerAsync({
-          useFactory: () => {
-            return {} as LdapModuleOptions;
-          },
-        }),
-
+        // ConfigModule.register('.env'),
+        // LdapModule.registerAsync({
+        //   useFactory: () => {
+        //     return {} as LdapModuleOptions;
+        //   },
+        // }),
         // #region TypeORM
         // TypeOrmModule.forRoot({}),
         // #endregion
-
-        ProfileModule,
+        // ProfileModule,
       ],
-      providers: [UserService, UserResolver, LdapService],
+      providers: [UserService, UserResolver],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
