e7e599fc95d21003690b055883075a977b16a02a
Roman Tikhiy <r.tikhiy@i-npz.ru>
2019-12-10 08:41:46 +0300

    fix eslint problem

--- apps/portal/lib/session-storage.ts
+++ apps/portal/lib/session-storage.ts
@@ -8,9 +8,9 @@
 export const getStorage = (name: string): string => (__SERVER__ ? '' : localStorage.getItem(name) || '');
 
 export const setStorage = (name: string, value: string): void => {
-  !__SERVER__ && localStorage.setItem(name, value);
+  if (!__SERVER__) localStorage.setItem(name, value);
 };
 
 export const removeStorage = (name: string): void => {
-  !__SERVER__ && localStorage.removeItem(name);
+  if (!__SERVER__) localStorage.removeItem(name);
 };
