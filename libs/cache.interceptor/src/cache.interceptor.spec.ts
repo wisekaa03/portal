d648678efd45453b547439d67b67a5221008f918
Stanislav V Vyaliy <stas@kngk-group.ru>
2019-12-19 18:21:10 +0300

    test cache

--- libs/cache.interceptor/src/cache.interceptor.spec.ts
+++ libs/cache.interceptor/src/cache.interceptor.spec.ts
@@ -4,14 +4,16 @@
 import * as CacheManager from 'cache-manager';
 // #endregion
 // #region Imports Local
-import { CacheInterceptor } from './cache.interceptor';
+import { LogService } from '@app/logger/logger.service';
+import { HttpCacheInterceptor } from './cache.interceptor';
 // #endregion
 
+jest.mock('@app/logger/logger.service');
 jest.mock('@nestjs/common/cache/interceptors/cache.interceptor');
 jest.mock('cache-manager');
 
 describe('CacheInterceptor', () => {
   it('should be defined', () => {
-    expect(new CacheInterceptor(CacheManager, {})).toBeDefined();
+    expect(new HttpCacheInterceptor(CacheManager, {}, new LogService())).toBeDefined();
   });
 });
