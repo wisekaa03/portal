698b75e5cd3e559a2e2941fb5522563fa95dfb3e
Roman Tikhiy <r.tikhiy@i-npz.ru>
2019-12-20 16:26:07 +0300

    utils: toBase64

---
+++ apps/portal/components/utils.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+export const toBase64 = (file: Blob): Promise<string | ArrayBuffer> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
