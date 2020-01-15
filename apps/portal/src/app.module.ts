05d383a1a33674bcab41effce4c83edab164e358
Stanislav V Vyaliy <stas@kngk-group.ru>
2020-01-15 09:52:13 +0300

    database redis cache

--- apps/portal/src/app.module.ts
+++ apps/portal/src/app.module.ts
@@ -149,14 +149,13 @@ const env = resolve(__dirname, dev ? (test ? '../../..' : '../../../..') : '../.
             TicketsModule, TicketAttachmentsModule, TicketCommentsModule
           ],
           migrationsRun: configService.get<boolean>('DATABASE_MIGRATIONS_RUN'),
-          cache: false,
-          // {
-          //   type: 'redis',
-          //   options: {
-          //     url: configService.get<string>('DATABASE_REDIS_URI'),
-          //   },
-          //   duration: configService.get<number>('DATABASE_REDIS_TTL'),
-          // },
+          cache: {
+            type: 'redis',
+            options: {
+              url: configService.get<string>('DATABASE_REDIS_URI'),
+            },
+            duration: configService.get<number>('DATABASE_REDIS_TTL'),
+          },
         } as TypeOrmModuleOptions;
       },
     }),
