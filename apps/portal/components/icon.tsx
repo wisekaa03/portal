7c6c6cabb7f2e5a2f715e775aa6fe60845d0f3f5
Roman Tikhiy <r.tikhiy@i-npz.ru>
2020-01-27 08:31:50 +0300

    services: next variant

--- apps/portal/components/icon.tsx
+++ apps/portal/components/icon.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 
+import NoImage from '../../../public/images/svg/noimage.svg';
+
 const iconWidth = 24;
 
 interface IconProps {
   src?: any;
-  base64?: string;
+  base64?: boolean;
   size?: number;
   material?: boolean;
 }
@@ -31,13 +33,15 @@ const BaseIcon = ({ size, ...props }: IconProps): React.ReactElement => {
     return <TheIcon />;
   }
 
-  if (props.base64) {
-    otherProps.src = `data:image/png;base64,${props.base64}`;
+  if (props.src) {
+    otherProps.src = props.base64 ? `data:image/png;base64,${props.src}` : props.src;
+  } else {
+    otherProps.src = NoImage;
   }
 
   return (
     <Icon>
-      <img className={classes.root} alt="icon" {...props} {...otherProps} />
+      <img className={classes.root} alt="icon" {...otherProps} />
     </Icon>
   );
 };
--- apps/portal/pages/services.tsx
+++ apps/portal/pages/services.tsx
@@ -21,6 +21,7 @@ import { useQuery } from '@apollo/react-hooks';
 import clsx from 'clsx';
 // #endregion
 // #region Imports Local
+import { OldService } from '@app/portal/ticket/old-service/models/old-service.interface';
 import Dropzone from '../components/dropzone';
 import { DropzoneFile } from '../components/dropzone/types';
 import { appBarHeight } from '../components/app-bar';
@@ -73,10 +74,10 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     serviceBox: {
       display: 'grid',
-      gap: `0 ${theme.spacing(4)}px`,
-      [theme.breakpoints.up('md')]: {
-        gridTemplateColumns: '1fr 2fr',
-      },
+      // gap: `0 ${theme.spacing(4)}px`,
+      // [theme.breakpoints.up('md')]: {
+      //   gridTemplateColumns: '1fr 2fr',
+      // },
     },
     service: {
       'padding': theme.spacing(),
@@ -127,17 +128,44 @@ const departments = [
   },
 ];
 
+interface CurrentProps {
+  id: number | string;
+  name: string;
+  icon: any;
+}
+
+interface TicketProps {
+  department: false | CurrentProps;
+  service: false | CurrentProps;
+  category: false | CurrentProps;
+  title: string;
+  text: string;
+}
+
+const defaultTicketState: TicketProps = {
+  department: false,
+  service: false,
+  category: false,
+  title: '',
+  text: '',
+};
+
 const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
   const classes = useStyles({});
-  const [currentTab, setCurrentTab] = useState(0);
-  const [departmentIndex, setDepartmentIndex] = useState<number>(-1);
-  const [serviceIndex, setServiceIndex] = useState<string>('');
-  const [category, setCategory] = useState<string>('');
-  const [title, setTitle] = useState<string>('');
-  const [text, setText] = useState<string>('');
+  const [currentTab, setCurrentTab] = useState<number>(0);
+  const [services, setServices] = useState<false | OldService[]>(false);
+  const [ticket, setTicket] = useState<TicketProps>(defaultTicketState);
   const [files, setFiles] = useState<DropzoneFile[]>([]);
 
   const { loading, data, error } = useQuery(SERVICES);
+
+  const handleTicket = (key: keyof TicketProps, value: any, tabIndex?: number): void => {
+    setTicket({ ...ticket, [key]: value });
+
+    if (tabIndex) {
+      setCurrentTab(tabIndex);
+    }
+  };
 
   const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number): void => {
     setCurrentTab(newValue);
@@ -147,52 +175,58 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
     setCurrentTab(index);
   };
 
-  const handleCurrentDepartment = (index: number) => (): void => {
-    setDepartmentIndex(index);
-    setCurrentTab(1);
-  };
-
-  const handleCurrentService = (index: string) => (): void => {
-    setServiceIndex(index);
-    setCurrentTab(2);
-  };
-
-  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setTitle(event.target.value);
-  };
-
-  const handleText = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setText(event.target.value);
-  };
-
-  const handleCategory = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setCategory(event.target.value);
+  const handleClearTicket = (): void => {
+    setTicket(defaultTicketState);
+    setCurrentTab(0);
   };
 
   const handleAccept = (): void => {};
 
-  const handleClose = (): void => {
-    setDepartmentIndex(-1);
-    setServiceIndex('');
-    setCurrentTab(0);
-    setTitle('');
-    setText('');
-    setFiles([]);
-    setCategory('');
-  };
-
-  const inputLabel = useRef<HTMLLabelElement>(null);
-  const [labelWidth, setLabelWidth] = useState(0);
-
-  const currentDepartment = departmentIndex >= 0 ? departments[departmentIndex] : false;
-  const currentService =
-    data && data.OldTicketService ? data.OldTicketService.find((service) => service.code === serviceIndex) : false;
-
   useEffect(() => {
-    if (inputLabel.current) {
-      setLabelWidth(inputLabel.current!.offsetWidth);
-    }
-  }, [inputLabel, currentService]);
+    setServices(!loading && !error && data && data.OldTicketService);
+  }, [loading, data, error]);
+
+  // const [departmentIndex, setDepartmentIndex] = useState<number>(-1);
+  // const [serviceIndex, setServiceIndex] = useState<string>('');
+  // const [category, setCategory] = useState<string>('');
+  // const [title, setTitle] = useState<string>('');
+  // const [text, setText] = useState<string>('');
+  // const [files, setFiles] = useState<DropzoneFile[]>([]);
+
+  // const handleCurrentDepartment = (index: number) => (): void => {
+  //   setDepartmentIndex(index);
+  //   setCurrentTab(1);
+  // };
+
+  // const handleCurrentService = (index: string) => (): void => {
+  //   setServiceIndex(index);
+  //   setCurrentTab(2);
+  // };
+
+  // const handleTitle = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  //   setTitle(event.target.value);
+  // };
+
+  // const handleText = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  //   setText(event.target.value);
+  // };
+
+  // const handleCategory = (event: ReahangeEvent<HTMLInputElement>): void => {
+  //   setCategory(event.target.value);
+  // };
+
+  // const inputLabel = useRef<HTMLLabelElement>(null);
+  // const [labelWidth, setLabelWidth] = useState(0);
+
+  // const currentDepartment = departmentIndex >= 0 ? departments[departmentIndex] : false;
+  // const currentService =
+  //   data && data.OldTicketService ? data.OldTicketService.find((service) => service.code === serviceIndex) : false;
+
+  // useEffect(() => {
+  //   if (inputLabel.current) {
+  //     setLabelWidth(inputLabel.current!.offsetWidth);
+  //   }
+  // }, [inputLabel, currentService]);
 
   const swipeableViews = useRef(null);
 
@@ -216,14 +250,15 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
           <Paper ref={tabHeader} square className={classes.header}>
             <Tabs value={currentTab} indicatorColor="primary" textColor="primary" onChange={handleTabChange}>
               <Tab label={t('services:tabs.tab1')} />
-              <Tab disabled={!currentDepartment} label={t('services:tabs.tab2')} />
-              <Tab disabled={!currentService} label={t('services:tabs.tab3')} />
+              <Tab disabled={!ticket.department} label={t('services:tabs.tab2')} />
+              <Tab disabled={!ticket.service} label={t('services:tabs.tab3')} />
+              <Tab disabled={!ticket.category} label={t('services:tabs.tab4')} />
             </Tabs>
           </Paper>
           <SwipeableViews
             ref={swipeableViews}
             animateHeight
-            disabled={!currentDepartment}
+            disabled={!ticket.department}
             index={currentTab}
             className={classes.contentWrap}
             containerStyle={{ flexGrow: 1 }}
@@ -233,9 +268,19 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
               {departments.map((department) => (
                 <Box
                   key={department.id}
-                  onClick={handleCurrentDepartment(department.id)}
+                  onClick={() =>
+                    handleTicket(
+                      'department',
+                      {
+                        id: department.id,
+                        name: department.title,
+                        icon: department.icon,
+                      },
+                      1,
+                    )
+                  }
                   className={clsx(classes.service, {
-                    [classes.serviceIndex]: departmentIndex === department.id,
+                    [classes.serviceIndex]: ticket.department && ticket.department.id === department.id,
                   })}
                 >
                   <div>
@@ -248,19 +293,27 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
               ))}
             </div>
             <div style={{ minHeight: containerHeight }} className={classes.container1}>
-              {currentDepartment &&
-                data &&
-                data.OldTicketService &&
-                data.OldTicketService.map((service) => (
+              {services &&
+                services.map((service) => (
                   <Box
                     key={service.code}
-                    onClick={handleCurrentService(service.code)}
+                    onClick={() =>
+                      handleTicket(
+                        'service',
+                        {
+                          id: service.code,
+                          name: service.name,
+                          icon: service.avatar,
+                        },
+                        2,
+                      )
+                    }
                     className={clsx(classes.service, {
-                      [classes.serviceIndex]: serviceIndex === service.code,
+                      [classes.serviceIndex]: ticket.service && ticket.service.id === service.code,
                     })}
                   >
                     <div>
-                      <BaseIcon base64={service.avatar} size={48} />
+                      <BaseIcon base64 src={service.avatar} size={48} />
                     </div>
                     <div>
                       <Typography variant="subtitle1">{service.name}</Typography>
@@ -268,28 +321,68 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
                   </Box>
                 ))}
             </div>
+            <div style={{ minHeight: containerHeight }} className={classes.container1}>
+              {services &&
+                ticket.service &&
+                services
+                  .find((service) => ticket.service && service.code === ticket.service.id)
+                  .category.map((category) => (
+                    <Box
+                      key={category.code}
+                      onClick={() =>
+                        handleTicket(
+                          'category',
+                          {
+                            id: category.code,
+                            name: category.name,
+                            icon: category.avatar,
+                          },
+                          3,
+                        )
+                      }
+                      className={clsx(classes.service, {
+                        [classes.serviceIndex]: ticket.category && ticket.category.id === category.code,
+                      })}
+                    >
+                      <div>
+                        <BaseIcon base64 src={category.avatar} size={48} />
+                      </div>
+                      <div>
+                        <Typography variant="subtitle1">{category.name}</Typography>
+                      </div>
+                    </Box>
+                  ))}
+            </div>
             <div style={{ minHeight: containerHeight }} className={classes.container2}>
-              {currentDepartment && currentService && (
+              {ticket.department && ticket.service && ticket.category && (
                 <div className={clsx(classes.serviceBox, classes.formControl)}>
                   <Box className={classes.service}>
                     <div>
-                      <BaseIcon src={currentDepartment.icon} size={48} />
+                      <BaseIcon src={ticket.department.icon} size={48} />
                     </div>
                     <div>
-                      <Typography variant="subtitle1">{currentDepartment.title}</Typography>
+                      <Typography variant="subtitle1">{ticket.department.name}</Typography>
                     </div>
                   </Box>
                   <Box className={classes.service}>
                     <div>
-                      <BaseIcon base64={currentService.avatar} size={48} />
+                      <BaseIcon base64 src={ticket.service.icon} size={48} />
                     </div>
                     <div>
-                      <Typography variant="subtitle1">{currentService.name}</Typography>
+                      <Typography variant="subtitle1">{ticket.service.name}</Typography>
+                    </div>
+                  </Box>
+                  <Box className={classes.service}>
+                    <div>
+                      <BaseIcon base64 src={ticket.category.icon} size={48} />
+                    </div>
+                    <div>
+                      <Typography variant="subtitle1">{ticket.category.name}</Typography>
                     </div>
                   </Box>
                 </div>
               )}
-              {currentService && (
+              {/* currentService && (
                 <FormControl className={classes.formControl} variant="outlined">
                   <InputLabel ref={inputLabel}>{t('services:form.category')}</InputLabel>
                   <Select value={category} onChange={handleCategory} labelWidth={labelWidth}>
@@ -300,11 +393,11 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
                     ))}
                   </Select>
                 </FormControl>
-              )}
+                    ) */}
               <FormControl className={classes.formControl} variant="outlined">
                 <TextField
-                  value={title}
-                  onChange={handleTitle}
+                  value={ticket.title}
+                  onChange={(e) => handleTicket('title', e.target.value)}
                   type="text"
                   label={t('services:form.title')}
                   variant="outlined"
@@ -312,8 +405,8 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
               </FormControl>
               <FormControl className={classes.formControl} variant="outlined">
                 <TextField
-                  value={text}
-                  onChange={handleText}
+                  value={ticket.text}
+                  onChange={(e) => handleTicket('text', e.target.value)}
                   multiline
                   rows={10}
                   type="text"
@@ -325,7 +418,7 @@ const Services: I18nPage = ({ t, ...rest }): React.ReactElement => {
                 <Dropzone setFiles={setFiles} files={files} {...rest} />
               </FormControl>
               <FormControl className={clsx(classes.formControl, classes.formAction)}>
-                <Button actionType="cancel" onClick={handleClose}>
+                <Button actionType="cancel" onClick={handleClearTicket}>
                   {t('common:cancel')}
                 </Button>
                 <Button onClick={handleAccept}>{t('common:accept')}</Button>
