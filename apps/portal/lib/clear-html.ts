78c7775c70b69b8751f9941ce00722d21c24c39c
Roman Tikhiy <r.tikhiy@i-npz.ru>
2020-02-07 14:08:56 +0300

    clear-html: substring

--- apps/portal/lib/clear-html.ts
+++ apps/portal/lib/clear-html.ts
@@ -5,7 +5,14 @@ const tagMeta = /<meta((.|\n|\r)*?)>/gim;
 
 const excludeTags = [tagStyle, tagMeta];
 
-const clearHtml = (html: string): string =>
-  excludeTags.reduce((acc: string, cur: RegExp): string => acc.replace(cur, ''), html);
+const clearHtml = (html: string, len?: number): string => {
+  const result = excludeTags.reduce((acc: string, cur: RegExp): string => acc.replace(cur, ''), html);
+
+  if (len) {
+    return result.substring(0, len);
+  }
+
+  return result;
+};
 
 export default clearHtml;
