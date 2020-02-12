930e2b4573316ffd3a82b7291eb5d0a5bd204a2f
Stanislav V Vyaliy <stas@kngk-group.ru>
2020-02-12 11:21:19 +0300

    pages -> media.edit

--- apps/portal/pages/media/edit.tsx
+++ apps/portal/pages/media/edit.tsx
@@ -11,7 +11,7 @@ import { Box, IconButton } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 // #endregion
 // #region Imports Local
-import Button from '../../components/common/button';
+import Button from '../../components/ui/button';
 import Page from '../../layouts/main';
 import { includeDefaultNamespaces, nextI18next, I18nPage } from '../../lib/i18n-client';
 import { MEDIA_EDIT, MEDIA } from '../../lib/queries';
