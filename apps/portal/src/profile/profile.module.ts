5b6d4c78ce457012adb3998156203eef72cfcfc8
Stanislav V Vyaliy <stas@kngk-group.ru>
2020-02-13 10:30:27 +0300

    profile module

--- apps/portal/src/profile/profile.module.ts
+++ apps/portal/src/profile/profile.module.ts
@@ -3,7 +3,6 @@
 // #region Imports NPM
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { I18nModule } from 'nestjs-i18n';
 // #endregion
 // #region Imports Local
 import { ConfigModule } from '@app/config';
