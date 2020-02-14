a550401d70cc1cf83d67691c89b7a73729e19c55
Roman Tikhiy <r.tikhiy@i-npz.ru>
2020-02-14 09:37:52 +0300

    fix box ref

--- apps/portal/lib/box-ref.ts
+++ apps/portal/lib/box-ref.ts
@@ -1,6 +1,6 @@
 /** @format */
 
 import React from 'react';
-import Box, { BoxProps } from '@material-ui/core';
+import Box, { BoxProps } from '@material-ui/core/Box';
 
 export default (Box as unknown) as React.ComponentType<{ ref: React.Ref<any> } & BoxProps>;
