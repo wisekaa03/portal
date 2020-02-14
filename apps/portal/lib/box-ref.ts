7dcdc0cd6727c755772707104b0329f7b3a17ffb
Roman Tikhiy <r.tikhiy@i-npz.ru>
2020-02-14 08:53:23 +0300

    box with ref

---
+++ apps/portal/lib/box-ref.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+import React from 'react';
+import Box, { BoxProps } from '@material-ui/core';
+
+export default (Box as unknown) as React.ComponentType<{ ref: React.Ref<any> } & BoxProps>;
