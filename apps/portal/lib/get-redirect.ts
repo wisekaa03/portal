69d715e6699525d002fc40690902993080a59d8d
Stanislav V Vyaliy <stas@kngk-group.ru>
2020-02-19 16:53:39 +0300

    getRedirect cosmetic changes

--- apps/portal/lib/get-redirect.ts
+++ apps/portal/lib/get-redirect.ts
@@ -2,12 +2,10 @@
 
 import { FIRST_PAGE, ALLOW_REDIRECT_PAGES } from './constants';
 
-const redirect = (pathname: any): string => {
+export default (pathname: any): string => {
   if (pathname && typeof pathname === 'string' && ALLOW_REDIRECT_PAGES.some((p) => pathname.startsWith(p))) {
     return pathname;
   }
 
   return FIRST_PAGE;
 };
-
-export default redirect;
