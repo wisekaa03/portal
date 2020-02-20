b1ae527d9f2b458ab409d2a8225b6823383fcf24
Roman Tikhiy <r.tikhiy@i-npz.ru>
2020-02-20 14:52:35 +0300

    app-bar/drawer: rebuild

---
+++ apps/portal/components/app-bar/index.tsx
@@ -0,0 +1,185 @@
+/** @format */
+
+// #region Imports NPM
+import React, { FC } from 'react';
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Popover, Box, Button, IconButton, Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import MenuIcon from '@material-ui/icons/Menu';
+import PhoneIcon from '@material-ui/icons/Phone';
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+// #endregion
+// #region Imports Local
+import HeaderBg from '../../public/images/jpeg/header_bg.jpg';
+import PopoverBg from '../../public/images/png/profile_popover_bg.png';
+import LogoMin from '../../public/images/png/logo_min.png';
+import { useTranslation } from '../../lib/i18n-client';
+import { ProfileContext } from '../../lib/context';
+import Avatar from '../ui/avatar';
+import { AppBarComponentProps } from './types';
+
+// #endregion
+
+const avatarHeight = 48;
+export const appBarHeight = 64;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      zIndex: theme.zIndex.drawer + 1,
+      background: `url(${HeaderBg})`,
+      backgroundSize: 'cover',
+    },
+    toolbar: {
+      padding: theme.spacing(0, 2),
+    },
+    menuButton: {
+      color: 'rgba(0, 0, 0, 0.54)',
+    },
+    logo: {
+      'flexGrow': 1,
+      '& > img': {
+        height: appBarHeight,
+      },
+    },
+    title: {
+      flexGrow: 1,
+    },
+    avatar: {
+      height: avatarHeight,
+      width: avatarHeight,
+    },
+    profile: {
+      padding: theme.spacing(0.5),
+      background: `url(${PopoverBg})`,
+      minWidth: '200px',
+      minHeight: '150px',
+      display: 'grid',
+      gridTemplateColumns: `200px ${avatarHeight}px`,
+      borderRadius: theme.spacing(0.5),
+    },
+    pointer: {
+      cursor: 'pointer',
+    },
+    avatarWrap: {
+      padding: theme.spacing(0.5),
+    },
+    profileName: {
+      margin: theme.spacing(),
+      fontSize: '16px',
+    },
+    commonBlock: {
+      display: 'grid',
+      gridColumn: '1 / 3',
+      gap: `${theme.spacing()}px`,
+      padding: theme.spacing(),
+    },
+    phoneBlock: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 6fr',
+      gap: `${theme.spacing()}px 0`,
+      alignItems: 'center',
+    },
+    buttonLogout: {
+      // backgroundColor: blue[300],
+    },
+  }),
+);
+
+const AppBarComponent: FC<AppBarComponentProps> = ({
+  open,
+  anchorEl,
+  handleDrawerOpen,
+  handlePopoverOpen,
+  handlePopoverClose,
+  handleLogout,
+}) => {
+  const classes = useStyles({});
+  const { t } = useTranslation();
+
+  return (
+    <AppBar id="header" position="sticky" className={classes.root}>
+      <Toolbar className={classes.toolbar}>
+        <ProfileContext.Consumer>
+          {({ user }) => (
+            <>
+              {user && (
+                <>
+                  <IconButton
+                    edge="start"
+                    onClick={handleDrawerOpen}
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="menu"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <div className={classes.logo}>
+                    <img src={LogoMin} alt="logo" />
+                  </div>
+                  <Box id="profile-avatar" className={classes.avatarWrap} onClick={handlePopoverOpen}>
+                    <Avatar className={clsx(classes.avatar, classes.pointer)} profile={user.profile} alt="photo" />
+                  </Box>
+                  <Popover
+                    id="profile-popover"
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handlePopoverClose}
+                    classes={{ paper: classes.profile }}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    marginThreshold={0}
+                    transitionDuration={0}
+                    disableRestoreFocus
+                  >
+                    <Typography className={classes.profileName}>{user.profile.fullName}</Typography>
+                    <Avatar className={classes.avatar} profile={user.profile} alt="photo" />
+                    <Box className={classes.commonBlock}>
+                      <Box className={classes.phoneBlock}>
+                        {user.profile.telephone && (
+                          <>
+                            <PhoneIcon />
+                            <Typography>{user.profile.telephone}</Typography>
+                          </>
+                        )}
+                        {user.profile.mobile && (
+                          <>
+                            <PhoneIphoneIcon />
+                            <Typography>{user.profile.mobile}</Typography>
+                          </>
+                        )}
+                        {user.profile.workPhone && (
+                          <>
+                            <PhoneInTalkIcon />
+                            <Typography>{user.profile.workPhone}</Typography>
+                          </>
+                        )}
+                      </Box>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.buttonLogout}
+                        onClick={handleLogout}
+                      >
+                        {t('common:signOut')}
+                      </Button>
+                    </Box>
+                  </Popover>
+                </>
+              )}
+            </>
+          )}
+        </ProfileContext.Consumer>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppBarComponent;
--- apps/portal/layouts/main.tsx
+++ apps/portal/layouts/main.tsx
@@ -1,28 +1,23 @@
 /** @format */
 
 // #region Imports NPM
-import React, { FC, useState, ReactNode, useEffect, useContext } from 'react';
+import React, { FC, useState, useEffect, useContext } from 'react';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
 import { useRouter } from 'next/router';
-import { useMutation } from '@apollo/react-hooks';
 import { Box, useMediaQuery } from '@material-ui/core';
 import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
 // #endregion
 // #region Imports Local
-import { nextI18next } from '../lib/i18n-client';
 import AppBarComponent, { appBarHeight } from '../components/app-bar';
 import DrawerComponent from '../components/drawer';
 import { ProfileContext } from '../lib/context';
-import { USER_SETTINGS } from '../lib/queries';
-import { AUTO_COLLAPSE_ROUTES } from '../lib/constants';
+import { LOGOUT, USER_SETTINGS } from '../lib/queries';
+import { removeStorage } from '../lib/session-storage';
+import { SESSION, FIRST_PAGE, AUTH_PAGE, AUTO_COLLAPSE_ROUTES } from '../lib/constants';
 // #endregion
 
 const useStyles = makeStyles((/* theme: Theme */) =>
   createStyles({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-    },
     content: {
       'flex': 1,
       'display': 'flex',
@@ -36,39 +31,36 @@ const useStyles = makeStyles((/* theme: Theme */) =>
     },
   }));
 
-interface MainLayoutProps {
-  children: ReactNode;
-}
-
-const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+const MainLayout: FC = ({ children }) => {
   const classes = useStyles({});
-  const theme = useTheme();
+
   const profile = useContext(ProfileContext);
+  const drawer = profile?.user?.settings.drawer;
+  const { isMobile } = profile;
 
-  const router = useRouter();
-  const isCollapse = router?.pathname && AUTO_COLLAPSE_ROUTES.some((r) => router.pathname.startsWith(r));
-
+  const theme = useTheme();
   const lgUp = useMediaQuery(theme.breakpoints.up('lg'));
   const ifModal = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const drawer = profile?.user?.settings.drawer as boolean | null;
-  const { isMobile } = profile;
-  const defaultDrawer = !isMobile && !ifModal && !isCollapse && (drawer !== null ? drawer : lgUp);
+  const client = useApolloClient();
+  const router = useRouter();
+  const isCollapse = router.pathname && AUTO_COLLAPSE_ROUTES.some((r) => router.pathname.startsWith(r));
+
+  const defaultDrawer = !isMobile && !ifModal && !isCollapse && (drawer !== undefined ? drawer : lgUp);
   const [drawerOpen, setDrawerOpen] = useState<boolean>(defaultDrawer);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const [userSettings] = useMutation(USER_SETTINGS);
 
-  useEffect(() => {
-    if (isMobile || ifModal) {
-      setDrawerOpen(false);
-    }
-  }, [isMobile, ifModal]);
+  const [logout] = useMutation(LOGOUT, {
+    onCompleted() {
+      removeStorage(SESSION);
+      client.resetStore();
 
-  useEffect(() => {
-    if (!isMobile && !ifModal && !isCollapse) {
-      setDrawerOpen(drawer === null ? lgUp : drawer);
-    }
-  }, [lgUp, drawer, isMobile, ifModal, isCollapse]);
+      const { pathname = FIRST_PAGE } = router;
+      router.push({ pathname: AUTH_PAGE, query: { redirect: pathname } });
+    },
+  });
 
   const handleDrawerOpen = (): void => {
     if (!isMobile && !ifModal) {
@@ -81,16 +73,47 @@ const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     setDrawerOpen(!drawerOpen);
   };
 
+  const handleLogout = (): void => {
+    logout();
+  };
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = (): void => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    if (isMobile || ifModal) {
+      setDrawerOpen(false);
+    }
+  }, [isMobile, ifModal]);
+
+  useEffect(() => {
+    if (!isMobile && !ifModal && !isCollapse) {
+      setDrawerOpen(drawer !== undefined ? drawer : lgUp);
+    }
+  }, [lgUp, drawer, isMobile, ifModal, isCollapse]);
+
   return (
-    <div className={classes.root}>
-      <AppBarComponent handleDrawerOpen={handleDrawerOpen} />
+    <Box display="flex" flexDirection="column" style={{ height: '100vh' }}>
+      <AppBarComponent
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        handleDrawerOpen={handleDrawerOpen}
+        handlePopoverOpen={handlePopoverOpen}
+        handlePopoverClose={handlePopoverClose}
+        handleLogout={handleLogout}
+      />
       <Box display="flex" flexGrow={1}>
         <DrawerComponent open={drawerOpen} isMobile={isMobile} handleOpen={handleDrawerOpen} />
-        <div id="content" className={classes.content}>
+        <Box id="content" className={classes.content}>
           {children}
-        </div>
+        </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
