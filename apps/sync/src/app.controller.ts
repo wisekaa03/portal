8137703d7ee0338f4d352ca307013f3b095d8ca2
Stanislav V Vyaliy <stas@i-npz.ru>
2020-03-26 17:59:58 +0300

    LDAP_SYNC fix

--- apps/sync/src/app.controller.ts
+++ apps/sync/src/app.controller.ts
@@ -5,7 +5,7 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 // #endregion
 // #region Imports Local
-import { LDAP_SYNC } from '@lib/types';
+import { LDAP_SYNC } from '@lib/constants';
 import { SyncService } from './app.service';
 // #endregion
 
