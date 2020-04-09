735e50f2b232e6d5a7831f1d7db0931de05b1973
Stanislav V Vyaliy <stas@i-npz.ru>
2020-04-10 02:17:01 +0300

    localStorage -> sessionStorage

--- apps/portal/lib/session-storage.ts
+++ apps/portal/lib/session-storage.ts
@@ -5,12 +5,12 @@
 // #region Imports Local
 // #endregion
 
-export const getStorage = (name: string): string => (__SERVER__ ? '' : localStorage.getItem(name) || '');
+export const getStorage = (name: string): string => (__SERVER__ ? '' : sessionStorage.getItem(name) || '');
 
 export const setStorage = (name: string, value: string): void => {
-  if (!__SERVER__) localStorage.setItem(name, value);
+  if (!__SERVER__) sessionStorage.setItem(name, value);
 };
 
 export const removeStorage = (name: string): void => {
-  if (!__SERVER__) localStorage.removeItem(name);
+  if (!__SERVER__) sessionStorage.removeItem(name);
 };
