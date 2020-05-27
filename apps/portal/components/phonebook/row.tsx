47cfc65f3125c780e53002c4c98b03b6ccb58cd3
Stanislav V Vyaliy <stas@i-npz.ru>
2020-05-27 16:21:50 +0300

    front: phonebook (Ivan)

--- apps/portal/components/phonebook/row.tsx
+++ apps/portal/components/phonebook/row.tsx
@@ -51,90 +51,92 @@ const PhonebookRow: FC<ListChildComponentProps> = ({ index, style: { width, top,
   const cell = data.items[index].node;
 
   return (
-    <Link href={{ pathname: '/phonebook', query: { id: cell.id } }} as={`/phonebook/${cell.id}`}>
-      <TableRow
-        component="div"
-        className={classes.row}
-        style={{ ...rest, top: `${parseFloat(top as string) + PHONEBOOK_ROW_HEIGHT}px` }}
-        hover
-      >
-        {allColumns
-          .filter(({ name }) => columns.includes(name) && !PHONEBOOK_HIDDEN_COLS.includes(name))
-          .map((column) => {
-            const { name, defaultStyle, largeStyle } = column;
+    <TableRow
+      component="div"
+      className={classes.row}
+      style={{ ...rest, top: `${parseFloat(top as string) + PHONEBOOK_ROW_HEIGHT}px` }}
+      hover
+    >
+      {allColumns
+        .filter(({ name }) => columns.includes(name) && !PHONEBOOK_HIDDEN_COLS.includes(name))
+        .map((column) => {
+          const { name, defaultStyle, largeStyle } = column;
 
-            const cellStyle = {
-              height: PHONEBOOK_ROW_HEIGHT,
-              ...(largeWidth ? largeStyle : defaultStyle),
-            };
+          const cellStyle = {
+            height: PHONEBOOK_ROW_HEIGHT,
+            ...(largeWidth ? largeStyle : defaultStyle),
+          };
 
-            let cellData: ReactElement | string = '';
+          let cellData: ReactElement | string = '';
 
-            switch (name) {
-              case 'thumbnailPhoto40': {
-                cellData = <Avatar profile={cell} alt="photo" />;
-                break;
-              }
-
-              case 'lastName':
-                cellData = cell.fullName;
-                break;
-
-              case 'manager': {
-                if (!cell.manager) {
-                  break;
-                }
-
-                cellData = cell.manager.fullName;
-                break;
-              }
-
-              case 'nameeng':
-              case 'username':
-              case 'company':
-              case 'companyeng':
-              case 'department':
-              case 'departmenteng':
-              case 'otdel':
-              case 'otdeleng':
-              case 'title':
-              case 'positioneng':
-              case 'room':
-              case 'telephone':
-              case 'fax':
-              case 'mobile':
-              case 'workPhone':
-              case 'email':
-              case 'country':
-              case 'region':
-              case 'town':
-              case 'street': {
-                cellData = cell[name];
-                break;
-              }
-
-              default: {
-                break;
-              }
+          switch (name) {
+            case 'thumbnailPhoto40': {
+              cellData = (
+                <Link href={{ pathname: '/phonebook', query: { id: cell.id } }} as={`/phonebook/${cell.id}`}>
+                  <Avatar profile={cell} alt="photo" />
+                </Link>
+              );
+              break;
             }
 
-            return (
-              <TableCell
-                key={name}
-                component="div"
-                variant="body"
-                className={clsx(classes.cell, {
-                  [classes.disabled]: cell.disabled && name === 'lastName',
-                  [classes.notShowing]: cell.notShowing && name === 'lastName',
-                })}
-                style={cellStyle}
-              >
-                {cellData}
-              </TableCell>
-            );
-          })}
-      </TableRow>
-    </Link>
+            case 'lastName':
+              cellData = cell.fullName;
+              break;
+
+            case 'manager': {
+              if (!cell.manager) {
+                break;
+              }
+
+              cellData = cell.manager.fullName;
+              break;
+            }
+
+            case 'nameeng':
+            case 'username':
+            case 'company':
+            case 'companyeng':
+            case 'department':
+            case 'departmenteng':
+            case 'otdel':
+            case 'otdeleng':
+            case 'title':
+            case 'positioneng':
+            case 'room':
+            case 'telephone':
+            case 'fax':
+            case 'mobile':
+            case 'workPhone':
+            case 'email':
+            case 'country':
+            case 'region':
+            case 'town':
+            case 'street': {
+              cellData = cell[name];
+              break;
+            }
+
+            default: {
+              break;
+            }
+          }
+
+          return (
+            <TableCell
+              key={name}
+              component="div"
+              variant="body"
+              className={clsx(classes.cell, {
+                [classes.disabled]: cell.disabled && name === 'lastName',
+                [classes.notShowing]: cell.notShowing && name === 'lastName',
+              })}
+              style={cellStyle}
+            >
+              {cellData}
+            </TableCell>
+          );
+        })}
+    </TableRow>
   );
 };
 
