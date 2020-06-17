f16f66e085ce27cc2d6ec28030a29983b95319cc
Stanislav V Vyaliy <stas@i-npz.ru>
2020-06-17 13:47:52 +0300

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { jsWithTs: tsjPreset } = require('ts-jest/presets');
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require('./tsconfig');
@@ -10,6 +11,15 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['./jest.setup.ts'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json',
+      // 	Disable type-checking
+      isolatedModules: true,
+    },
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
     ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
@@ -17,26 +27,18 @@ module.exports = {
       '<rootDir>/apps/portal/__mocks__/fileMock.js',
   },
   transform: {
-    '^.+\\.[jt]s(x)?$': 'ts-jest',
+    ...tsjPreset.transform,
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.svg$': 'jest-svg-transformer',
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/', '!<rootDir>/node_modules/simple-git'],
+  transformIgnorePatterns: ['node_modules/(?!(simple-git/src))/'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/(*.)+(spec|test).[jt]s?(x)'],
-  setupFiles: ['./jest.setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/.git/',
     '<rootDir>/dist/',
     '<rootDir>/.next/',
     '<rootDir>/apps/portal/.next/',
     '<rootDir>/node_modules/',
-    '!<rootDir>/node_modules/simple-git',
     '<rootDir>/.local/',
   ],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.jest.json',
-    },
-  },
 };
