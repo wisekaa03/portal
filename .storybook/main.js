207820a4c6d095f34b7fee638987cb45e78fef49
Stanislav V Vyaliy <stas@i-npz.ru>
2020-06-17 17:39:48 +0300

    storybook: next-i18next

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 /** @format */
 
+const Webpack = require('webpack');
 const resolveTsconfigPaths = require('../tsconfig-paths-to-webpack-alias');
 
 module.exports = {
@@ -10,27 +11,35 @@ module.exports = {
     '@storybook/addon-actions/register',
     '@storybook/addon-links/register',
     'storybook-addon-i18n/register',
+    'storybook-addon-material-ui/register',
     '@storybook/addons',
     {
       name: '@storybook/preset-typescript',
       options: {
         tsLoaderOptions: {
-          configFile: '../apps/portal/tsconfig.json',
+          configFile: '../apps/portal/tsconfig.app.json',
         }
       }
     }
   ],
   webpackFinal: async (config) => {
 
+    config.plugins = [
+      ...(config.plugins || []),
+      new Webpack.IgnorePlugin({
+        resourceRegExp: /next-i18next/
+      }),
+    ];
+
     config.resolve = {
       ...(config.resolve || []),
       alias: {
         ...config.resolve.alias,
-        ...resolveTsconfigPaths({ tsconfigPaths: '../apps/portal/tsconfig.json' }),
+        ...resolveTsconfigPaths({ tsconfigPaths: '../apps/portal/tsconfig.app.json' }),
       },
     };
 
-    // console.log(config);
+    console.log(config);
 
     return config;
   },
