8e18ef55a9dc0e362d2d4d0de16b2c753014410a
Stanislav V Vyaliy <stas@i-npz.ru>
2020-09-11 10:42:43 +0300

    drawer testfix

--- apps/portal/components/drawer.test.tsx
+++ apps/portal/components/drawer.test.tsx
@@ -30,7 +30,7 @@ describe('Drawer component', () => {
     handleOpen: jest.fn(),
     namespacesRequired: [],
   };
-  const linkCount = 7;
+  const linkCount = 5;
   // const linkAdminCount = 12;
 
   describe('Without context', () => {
