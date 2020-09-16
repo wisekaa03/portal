0c741b033537f56234a42b4900365278c5f5c395
Stanislav V Vyaliy <stas@i-npz.ru>
2020-09-16 09:59:05 +0300

    docflow: dirty fix...

--- apps/portal/src/docflow/docflow.module.ts
+++ apps/portal/src/docflow/docflow.module.ts
@@ -12,21 +12,32 @@ import { DocFlowService } from './docflow.service';
 
 @Module({
   imports: [
-    SoapModule.registerAsync({
-      name: 'DOCFLOW',
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        url: configService.get<string>('DOCFLOW_URL'),
-        options: {
-          wsdl_headers: {
-            connection: 'keep-alive',
-          },
-          wsdl_options: {
-            ntlm: true,
-            domain: configService.get<string>('LDAP_DOMAIN'),
-          },
+    SoapModule.register({
+      url: 'http://10.10.205.3/dev_01/ws/dm.1cws?wsdl',
+      options: {
+        wsdl_headers: {
+          connection: 'keep-alive',
         },
-      }),
+        wsdl_options: {
+          ntlm: true,
+          domain: 'i-npz.ru',
+        },
+      },
+
+      // name: 'DOCFLOW',
+      // inject: [ConfigService],
+      // useFactory: async (configService: ConfigService) => ({
+      //   url: configService.get<string>('DOCFLOW_URL'),
+      //   options: {
+      //     wsdl_headers: {
+      //       connection: 'keep-alive',
+      //     },
+      //     wsdl_options: {
+      //       ntlm: true,
+      //       domain: configService.get<string>('LDAP_DOMAIN'),
+      //     },
+      //   },
+      // }),
     }),
     HttpModule,
   ],
