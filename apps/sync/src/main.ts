5f84c0e07a027f32217fe66dac4f3d65dc3d6071
Stanislav V Vyaliy <stas@i-npz.ru>
2020-09-27 10:31:53 +0300

    logger fiux

--- apps/sync/src/main.ts
+++ apps/sync/src/main.ts
@@ -13,18 +13,22 @@ import { AppModule } from './app.module';
 //#endregion
 
 async function bootstrap(config: ConfigService): Promise<void> {
-  const logger = new Logger(new PinoLogger(pinoOptions(config)), {});
+  let logger = new Logger(new PinoLogger(pinoOptions()), {});
 
-  const server = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     logger,
     transport: Transport.REDIS,
     options: {
       url: config.get<string>('MICROSERVICE_URL'),
     },
   });
-  server.useLogger(logger);
 
-  await server.listen(() => logger.log('Microservice is listening', 'Sync LDAP'));
+  logger = app.get(Logger);
+  const configService = app.get(ConfigService);
+  configService.logger = logger;
+  app.useLogger(logger);
+
+  await app.listen(() => logger.log('Microservice is listening', 'Sync LDAP'));
 }
 
 const configService = new ConfigService(resolve(__dirname, '../../..', '.local/.env'));
