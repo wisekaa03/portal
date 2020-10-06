3b5fb4acc4d4641ef598e3cf2517d8c330693d3d
Stanislav V Vyaliy <stas@i-npz.ru>
2020-10-06 16:52:08 +0300

    logger to graylog

--- apps/portal/src/shared/logger.options.ts
+++ apps/portal/src/shared/logger.options.ts
@@ -2,11 +2,35 @@
 
 import { utilities as nestWinstonModuleUtilities, WinstonModuleOptions } from 'nest-winston';
 import winston from 'winston';
+import { WinstonGraylog } from '@pskzcompany/winston-graylog';
+import { ConfigService } from '@app/config/config.service';
 
-export const winstonOptions = (): WinstonModuleOptions => ({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.timestamp(), nestWinstonModuleUtilities.format.nestLike()),
-    }),
-  ],
-});
+export const winstonOptions = (configService: ConfigService): WinstonModuleOptions => {
+  const level = configService.get<string>('LOG_LEVEL') || 'debug';
+  const graylog = configService.get<string>('LOG_SERVER');
+
+  const options = {
+    level,
+    exitOnError: false,
+    transports: [
+      new winston.transports.Console({
+        level,
+        format: winston.format.combine(winston.format.timestamp(), nestWinstonModuleUtilities.format.nestLike()),
+      }) as winston.transport,
+    ],
+  };
+
+  if (graylog) {
+    options.transports.push(
+      new WinstonGraylog({
+        level,
+        graylog,
+        defaultMeta: {
+          environment: __DEV__ ? 'development' : 'production',
+        },
+      }) as winston.transport,
+    );
+  }
+
+  return options;
+};
